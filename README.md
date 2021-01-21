0b9d10269a2e4556eaca83f65087cf298d7181d6
JoshKing56 <joshuaiking56@gmail.com>
2021-01-21 17:07:25 -0500

    added references

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# medusa
+# Medusa
 Scripts, configurations, etc for crypto miner
+## References
+### Python 
+- [Getting started with python and influx](https://www.influxdata.com/blog/getting-started-python-influxdb/)
+- [library I could maybe use to get around using telegraf](https://github.com/giampaolo/psutil)
+- [SO post I got ^ from, also suggested other tools](https://stackoverflow.com/questions/16326529/python-get-process-names-cpu-mem-usage-and-peak-mem-usage-in-windows)
+### Grafana
+- [How To Install and Secure Grafana on Ubuntu 20.04](https://www.digitalocean.com/community/tutorials/how-to-install-and-secure-grafana-on-ubuntu-20-04)
+### Windows side
+- [Configure and Run Telegraf (on Windows)](https://thesqldump.code.blog/2019/11/15/configure-and-run-telegraf-on-windows/)
+- [Influx dl page](https://portal.influxdata.com/downloads/)
+- [Enable HTTPS with InfluxDB](https://docs.influxdata.com/influxdb/v1.8/administration/https_setup/)
--- log_parser/parse_file.py
+++ log_parser/parse_file.py
@@ -12,7 +12,16 @@ def check_relevant(line):
     return True
 
 def parse_data(line):
-    return data
+    # TODO @james pls implemnt
+    # Sample lines
+    #   2021.01.11:17:31:54.389: main Eth speed: 80.770 MH/s, shares: 606/0/0, time: 7:33
+    #   2021.01.11:17:31:55.832: main Eth: Accepted shares 607 (0 stales), rejected shares 0 (0 stales)
+    #   2021.01.11:17:31:55.832: main Eth: Incorrect shares 0 (0.00%), est. stales percentage 0.00% 
+    #   2021.01.11:17:31:55.832: main Eth: Maximum difficulty of found share: 4008.0 GH (!)
+    #   2021.01.11:17:31:55.832: main Eth: Average speed (5 min): 79.442 MH/s 
+    #   GPUs power: 322.4 W
+    #   2021.01.11:17:31:34.580: main GPU1: 70C 65% 322W
+    return line 
 
 def write_to_db(line):
     return True
@@ -26,7 +35,7 @@ def main():
         for line in new_lines:
             if check_error(line):
                 report_error(line)
-
             elif check_relevant(line):
                 write_to_db(parse_data(line))
 
+# def test_stream_read():
