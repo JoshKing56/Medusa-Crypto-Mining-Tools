d1ce65bb2d3c0cf43e5c611d01e68abe972d7b81
JoshKing56 <joshuaiking56@gmail.com>
2021-01-21 19:37:00 -0500

    untested influx logic done

--- log_parser/constants.py
+++ log_parser/constants.py
@@ -1,4 +1,12 @@
 CONSTANTS = {
+    'NO_DB': True, # do not use the database logic, writes to output instead
     'FILE_READ_RATE_IN_SEC': 0.5,
     'LOG_FILEPATH': "../utils/stream_test.txt"
+ }
+INFLUX_INFO = {
+     'HOST': "localhost",
+     'PORT': 8086,
+     'USERNAME': "myuser",
+     'PASSWORD': "letmein",
+     'DBNAME': "medusa_stats"
  }
--- log_parser/parse_file.py
+++ log_parser/parse_file.py
@@ -1,5 +1,7 @@
-import os, time
+import os, time, 
+from influxdb import InfluxDBClient
 from constants import CONSTANTS
+from constants import INFLUX_INFO 
 
 def get_new_lines():
     return [] 
@@ -22,10 +24,19 @@ def parse_data(line):
     #   2021.01.11:17:31:34.580: main GPU1: 70C 65% 322W
     return f"jsonified {line}"
 
-def write_to_db(line):
-    print(f"Writing to db: {line}")
+def write_to_db(data, influx_client):
+    if CONSTANTS["NO_DB"]:
+        print(f"Writing to db: {line}")
+        return None
+    try:
+        influx_client.ping()
+    except:
+        report_error("PYTHON ERROR: CONNECTION TO INFLUX LOST")
+    if not influx_client.write_points(json):
+        report_error("PYTHON ERROR: COULD NOT WRITE TO DATABASE")
 
-def report_error(line): # TODO: handle errors separately
+def report_error(line):
+    # TODO: handle errors separately
     write_to_db(parse_data(line))
 
 def main():
@@ -33,6 +44,13 @@ def main():
     log_file = open(filename,'r')
     file_size = os.stat(filename)[6]
     log_file.seek(file_size)
+    influx = InfluxDBClient(host=INFLUX_INFO['HOST'],
+                            port=INFLUX_INFO['PORT'],
+                            username=INFLUX_INFO['USERNAME'],
+                            password=INFLUX_INFO['PASSWORD'])
+    dbname = INFLUX_INFO['DBNAME'] 
+    client.create_database(dbname) #going to assume this gets ignored if it already exists for now
+    client.switch_database(dbname)
     while True:
         where = log_file.tell()
         line = log_file.readline()
@@ -44,6 +62,6 @@ def main():
             if check_error(line):
                 report_error(line)
             elif check_relevant(line):
-                write_to_db(parse_data(line))
+                write_to_db(parse_data(line), influx)
 
 main()
